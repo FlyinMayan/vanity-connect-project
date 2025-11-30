5f982d4d87ae416cceba44f5bf875105a176ef04
Michael Gutierrez <michael.jordan.gutierrez@gmail.com>
2025-11-30 14:49:06 -0700

    add gh pages

--- vanity-frontend/vite.config.ts
+++ vanity-frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // important: use relative paths for assets
+  base: "/vanity-connect-project/", // important for GitHub Pages
 });
